4136ef48a9399dbebf4f798c338a2ae40bd12fd3
Parnell Polliotti <parnell@mediawind.be>
2022-10-24 15:22:40 +0200

    feat: added generic groupByTwo utils

---
+++ utils/groupByTwo.ts
@@ -0,0 +1,17 @@
+function groupByTwo<T>(array: any[]): T[][] {
+	let result = [] as any[];
+
+	array.forEach((item, index) => {
+		const normalizedIndex = Math.floor(index / 2);
+
+		if (result[normalizedIndex] === undefined) {
+			result[normalizedIndex] = [];
+		}
+
+		result[normalizedIndex].push(item);
+	});
+
+	return result;
+}
+
+export default groupByTwo;
