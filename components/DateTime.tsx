1285d929626695fea18081b17d4dc61aa8eca382
Noémie <noemie@mediawind.be>
2024-02-14 13:07:19 +0100

    feat: locale can dynamically change

--- components/DateTime.tsx
+++ components/DateTime.tsx
@@ -6,26 +6,37 @@ import { setIntervalSync } from "../customInterval";
 
 interface IDateTimeProps {
 	format: string
-	className?: string;
+	className?: string
+	divStyle?: React.CSSProperties
+	textStyle?: React.CSSProperties
 	locale?: string
 	updateInterval?: number
 }
 
 export default function DateTime(props: IDateTimeProps): JSX.Element {
-	const { format, className, locale, updateInterval, } = props;
+	const { format, className, divStyle, textStyle, locale, updateInterval, } = props;
 	const [dateTime, setDateTime] = useState<Dayjs>(dayjs());
 
-	dayjs.locale(locale ? locale : "fr");
+	dayjs.locale(locale ?? "fr");
 
 	useEffect(() => {
-		setIntervalSync(() => {
+		const delay = setIntervalSync(() => {
 			setDateTime(dayjs());
 		}, (updateInterval ?? 60) * 1000);
+
+		return () => {
+			clearInterval(delay);
+		};
 	}, []);
 
+	useEffect(() => {
+		dayjs.locale(locale ?? "fr");
+		setDateTime(dayjs());
+	}, [locale]);
+
 	return (
-		<div className={className ?? ""}>
-			<p>{dateTime.format(format)}</p>
+		<div className={className ?? ""} style={divStyle}>
+			<p style={textStyle}>{dateTime.format(format)}</p>
 		</div>
 	);
 }
