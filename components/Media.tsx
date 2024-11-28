ac1844ad4e2a9bad76bcf986fffab5f58420e289
Matthieu Schmit <matthieu@mediawind.be>
2024-11-28 11:23:44 +0100

    feat: enable video streaming

--- components/Media.tsx
+++ components/Media.tsx
@@ -4,8 +4,6 @@ import mime from "mime";
 
 import useHtml from "../hooks/useHtml";
 
-import { useEffect } from "react";
-
 interface IMediaProps {
 	UUID: string;
 	url: string;
@@ -16,20 +14,9 @@ interface IMediaProps {
 	right: number;
 	zIndex?: number;
 	muted: boolean;
-	width?: number;
-	height?: number;
 }
 
 function Media(props: IMediaProps): JSX.Element {
-	useEffect(() => {	
-		// Remove media element if they are generated outside the classic flow
-		return () => {
-			const element = document.getElementById(props.UUID);
-			if (element) {
-				element.remove();
-			}
-		};
-	},[]);
 
 	if (props.url == "") {
 		return <p>{props.emptyUrlError}</p>;
@@ -51,15 +38,6 @@ function Media(props: IMediaProps): JSX.Element {
 
 	if (container != null) {
 		container.classList.add("media");
-		container.style.position = "absolute";
-		container.style.top = `${props.top}%`;
-		container.style.bottom = `${props.bottom}%`;
-		container.style.left = `${props.left}%`;
-		container.style.right = `${props.right}%`;
-		container.style.zIndex = `${props.zIndex ? props.zIndex : 0}`;
-		container.style.width = `${props.width ? props.width : 100 - (props.left + props.right)}%`;
-		container.style.height = `${props.height ? props.height : 100 - (props.top + props.bottom)}%`;
-
 		container.innerHTML = "";
 
 		switch (type) {
@@ -80,10 +58,20 @@ function Media(props: IMediaProps): JSX.Element {
 
 				break;
 			case "video/mp4":
+			case "video/quicktime":
 				container.innerHTML = `<video src="${props.url}" autoPlay ${props.muted ? "muted" : ""} loop></video>`;
 
+				if (props.url.includes("http")) {
+					const videoHtml = container.getElementsByTagName("video")[0];
+					window.addEventListener("offline", () => {
+						videoHtml.pause();
+					});
+					window.addEventListener("online", () => {
+						videoHtml.play();
+					});
+				}
+
 				break;
-			case "image/gif":
 			case "image/png":
 			case "image/jpeg":
 				container.innerHTML = `<img src="${props.url}" />`;
@@ -96,9 +84,6 @@ function Media(props: IMediaProps): JSX.Element {
 		}
 	}
 
-	const width = props.width ? props.width : 100 - (props.left + props.right);
-	const height = props.height ? props.height : 100 - (props.top + props.bottom);
-
 	return <style>
 		{`
 			.media {
@@ -108,8 +93,8 @@ function Media(props: IMediaProps): JSX.Element {
 				left: ${props.left}%;
 				right: ${props.right}%;
 				z-index: ${props.zIndex ? props.zIndex : 0};
-				width: ${width}%;
-				height: ${height}%;
+				width: ${100 - (props.left + props.right)}%;
+				height: ${100 - (props.top + props.bottom)}%;
 			}
 		`}
 	</style>;
