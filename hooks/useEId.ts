3a96e723f04781c3461d7fdfa6e4555d5104449c
MaximeMediawind <maxime@mediawind.be>
2025-01-20 12:30:10 +0100

    feat: add new fields to eIdData interface for enhanced identity information

--- hooks/useEId.ts
+++ hooks/useEId.ts
@@ -23,6 +23,9 @@ export interface eIdData {
 	cardNumber: string,
 	validityBeginDate: string,
 	validityEndDate: string,
+	issuingMunicipality: string,
+	firstLetterOfThirdGivenName: string,
+	specialStatus: string,
 }
 
 const useEId = (
@@ -74,11 +77,10 @@ const useEId = (
 						cardNumber: result.data.eid.card_number,
 						validityBeginDate: result.data.eid.validity_begin_date,
 						validityEndDate: result.data.eid.validity_end_date,
-						chipNumber: result.data.eid.chip_number,
-						issuingMunicipality: result.data.eid.issuing_municipality,
-						firstLettterofThirdGivenName: result.data.eid.first_letter_of_third_given_name,
-						specialStatus: result.data.eid.special_status,
-						photoHash: result.data.eid.photo_hash,
+						chipNumber: result.data.eid.chip_number ?? "",
+						issuingMunicipality: result.data.eid.issuing_municipality ?? "",
+						specialStatus: result.data.eid.special_status ?? "",
+						firstLetterOfThirdGivenName: result.data.eid.first_letter_of_third_given_name ?? "",
 					} as eIdData);
 				} else {
 					setData(null);
